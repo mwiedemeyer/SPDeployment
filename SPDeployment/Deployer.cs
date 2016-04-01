6e6c64ec927a77fedb98a911576dc5e513817e78
Marco Wiedemeyer <mail@mwiedemeyer.de>
2016-04-01 16:25:31 +0200

    Fix to make watch task case independent and add Include pattern to spdeployment.json

--- SPDeployment/Deployer.cs
+++ SPDeployment/Deployer.cs
@@ -116,6 +116,9 @@ namespace SPDeployment
                             string[] excludeSplit = null;
                             if (!string.IsNullOrEmpty(fileConfig.Exclude))
                                 excludeSplit = fileConfig.Exclude.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                            string[] includeSplit = null;
+                            if (!string.IsNullOrEmpty(fileConfig.Include))
+                                includeSplit = fileConfig.Include.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
 
                             var folderCache = new Dictionary<string, Folder>();
 
@@ -135,6 +138,23 @@ namespace SPDeployment
                                     if (excludeFile)
                                     {
                                         Log("...... {0} skipped by exclude pattern", ConsoleColor.DarkYellow, localFile);
+                                        continue;
+                                    }
+                                }
+                                if (includeSplit != null)
+                                {
+                                    var excludeFile = false;
+                                    foreach (var inc in includeSplit)
+                                    {
+                                        if (!Regex.Match(localFile, inc, RegexOptions.IgnoreCase).Success)
+                                        {
+                                            excludeFile = true;
+                                            break;
+                                        }
+                                    }
+                                    if (excludeFile)
+                                    {
+                                        Log("...... {0} skipped by include pattern", ConsoleColor.DarkYellow, localFile);
                                         continue;
                                     }
                                 }
@@ -198,7 +218,7 @@ namespace SPDeployment
                 fs.EnableRaisingEvents = true;
                 _watcherCache.Add(fs);
 
-                _registeredSources.Add(fileConfig.Source, new Tuple<DeploymentSite, DeploymentFile>(site, fileConfig));
+                _registeredSources.Add(fileConfig.Source.ToUpperInvariant(), new Tuple<DeploymentSite, DeploymentFile>(site, fileConfig));
             }
         }
 
@@ -221,9 +241,9 @@ namespace SPDeployment
                 Tuple<DeploymentSite, DeploymentFile> sourceFound = null;
                 while (sourceFound == null && dir != null)
                 {
-                    if (_registeredSources.ContainsKey(dir.Name))
+                    if (_registeredSources.ContainsKey(dir.Name.ToUpperInvariant()))
                     {
-                        sourceFound = _registeredSources[dir.Name];
+                        sourceFound = _registeredSources[dir.Name.ToUpperInvariant()];
                         break;
                     }
                     dir = dir.Parent;
@@ -239,6 +259,9 @@ namespace SPDeployment
                 string[] excludeSplit = null;
                 if (!string.IsNullOrEmpty(fileConfig.Exclude))
                     excludeSplit = fileConfig.Exclude.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] includeSplit = null;
+                if (!string.IsNullOrEmpty(fileConfig.Include))
+                    includeSplit = fileConfig.Include.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
 
                 if (excludeSplit != null)
                 {
@@ -254,7 +277,21 @@ namespace SPDeployment
                     if (excludeFile)
                         return;
                 }
-                                
+                if (includeSplit != null)
+                {
+                    var excludeFile = false;
+                    foreach (var inc in includeSplit)
+                    {
+                        if (!Regex.Match(localFile, inc, RegexOptions.IgnoreCase).Success)
+                        {
+                            excludeFile = true;
+                            break;
+                        }
+                    }
+                    if (excludeFile)
+                        return;
+                }
+
                 var filename = Path.GetFileName(localFile);
 
                 Log("...... Deploying {0}...", ConsoleColor.DarkGray, filename);
--- SPDeployment/DeploymentConfiguration.cs
+++ SPDeployment/DeploymentConfiguration.cs
@@ -27,6 +27,7 @@ namespace SPDeployment
         public string Source { get; set; }
         public string Destination { get; set; }
         public string Exclude { get; set; }
+        public string Include { get; set; }
     }
 
     /// <summary>
