af8b5ac8e5f5b32302ec214728a9f999faf76ca0
mwiedemeyer <marco@wiedemeyer.eu>
2017-02-21 13:48:49 +0100

    Chrome cookie caching

--- SPDeployment/CookieStore.cs
+++ SPDeployment/CookieStore.cs
@@ -13,32 +13,45 @@ namespace SPDeployment
 {
     internal class CookieStore
     {
+        private static readonly Dictionary<Uri, CookieContainer> s_cookieCache = new Dictionary<Uri, CookieContainer>();
+
         public static CookieContainer GetCookieContainer(Uri uri)
         {
-            CookieContainer cookies = new CookieContainer();
-
-            var cookieFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Google\\Chrome\\User Data\\Default\\Cookies");
-
-            var connString = "Data Source=" + cookieFile + ";pooling=false";
-            using (var conn = new SQLiteConnection(connString))
-            using (var cmd = conn.CreateCommand())
+            if (!s_cookieCache.ContainsKey(uri))
             {
-                cmd.CommandText = $"select * from cookies where host_key='{uri.DnsSafeHost}'";
-                conn.Open();
-                var reader = cmd.ExecuteReader();
-
-                while (reader.Read())
+                lock (s_cookieCache)
                 {
-                    var val = DecryptValue((byte[])reader["encrypted_value"]);
+                    if (!s_cookieCache.ContainsKey(uri))
+                    {
+                        CookieContainer cookies = new CookieContainer();
 
-                    var c = new Cookie(reader["name"]?.ToString(), val, reader["path"]?.ToString(), reader["host_key"]?.ToString());
-                    c.Secure = (long)reader["secure"] == 1 ? true : false;
-                    c.HttpOnly = (long)reader["httponly"] == 1 ? true : false;
-                    cookies.Add(c);
+                        var cookieFile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Google\\Chrome\\User Data\\Default\\Cookies");
+
+                        var connString = "Data Source=" + cookieFile + ";pooling=false";
+                        using (var conn = new SQLiteConnection(connString))
+                        using (var cmd = conn.CreateCommand())
+                        {
+                            cmd.CommandText = $"select * from cookies where host_key='{uri.DnsSafeHost}'";
+                            conn.Open();
+                            var reader = cmd.ExecuteReader();
+
+                            while (reader.Read())
+                            {
+                                var val = DecryptValue((byte[])reader["encrypted_value"]);
+
+                                var c = new Cookie(reader["name"]?.ToString(), val, reader["path"]?.ToString(), reader["host_key"]?.ToString());
+                                c.Secure = (long)reader["secure"] == 1 ? true : false;
+                                c.HttpOnly = (long)reader["httponly"] == 1 ? true : false;
+                                cookies.Add(c);
+                            }
+                        }
+
+                        s_cookieCache.Add(uri, cookies);
+                    }
                 }
             }
 
-            return cookies;
+            return s_cookieCache[uri];
         }
 
         private static string DecryptValue(byte[] encValue)
